b2ad30ef61f650f94fbf38b47712bde99979e7af
_17 <you@example.com>
2025-11-05 21:06:57 +0330

    session 24

--- students/models.py
+++ students/models.py
@@ -9,7 +9,7 @@ class Student (models.Model):
     field = models.CharField(max_length=256)
     grade = models.PositiveIntegerField(default=0)
 #    tasks = models.ForeignKey(Task)
-    user= models.ForeignKey(User, on_delete = models.CASCADE, related_name="student")
+    user= models.OneToOneField("Profile", on_delete = models.CASCADE, related_name="student", null = True)
     def __str__(self):
         return self.name
     
@@ -17,27 +17,26 @@ class Student (models.Model):
 class Teacher (models.Model):
     name = models.CharField(max_length=128)
     score = models.PositiveIntegerField(default=0)
-    user= models.ForeignKey(User, on_delete = models.CASCADE, related_name="teacher" )
+    user= models.OneToOneField("Profile", on_delete = models.CASCADE, related_name="teacher" , null = True)
     def __str__(self):
         return self.name
     
 
-activity_status = {
-    0 : "offline",
-    1 : "online",
-    2 : "invisible",
-    3 : "do not disturb"
-}
-role = { 1 : Student, 2 : Teacher }
+# activity_status = {
+#     0 : "offline",
+#     1 : "online",
+#     2 : "invisible",
+#     3 : "do not disturb"
+#}
+# role = { 1 : Student, 2 : Teacher }
 
 class Profile(models.Model):
     bio = models.TextField(max_length=512, default="")
-    username = models.CharField(max_length=32)
     phone_number = models.CharField (max_length=13, default="+989000000000")
     birthday = models.DateField()
-    # avatar = models.ImageField()
-    #belongsto = models.OneToOneField(role, on_delete=models.CASCADE)
-    status = models.PositiveIntegerField(choices=activity_status)
+    #avatar = models.ImageField(null=True, blank=True)
+    belongsto = models.OneToOneField(User, on_delete=models.CASCADE)
+    # status = models.PositiveIntegerField(choices=activity_status)
     
     def __str__(self):
         return f"{self.belongsto.name}'s Profile"
@@ -49,6 +48,7 @@ class Course(models.Model):
     start = models.DateField()
     end = models.DateField()
     students = models.ManyToManyField(Student, related_name="courses")
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL ,null=True, blank=True)
 
     def __str__(self):
         return self.title
